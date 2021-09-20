27a3e4f2968e1b31853a208569237cb688eafcf5
frosty03 <jatin.chourasia@supista.com>
2021-09-21 02:33:19 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
 import { Button, Buttons } from './button/Button'
+import styles from './styles.module.css'
 
+export const ExampleComponent = ({ text }) => {
+  return <div className={styles.test}>Example Component: {text}</div>
+}
 export { Button, Buttons }
